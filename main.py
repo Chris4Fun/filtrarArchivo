21013e78909c2171332a725ba0a4e0d036118eab
Christopher <christopheracostam14@gmail.com>
2024-05-17 14:10:17 -0600

    Ajustes finales

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
+#!/usr/bin/env python
+
 import os
 import re
-import json
+from datetime import datetime
 
 def main():
     analyzer()
     
 def analyzer():
-    # nombre_archivo = "FILTRADO.txt"
-    # with open(nombre_archivo, "w") as outputFile:
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    nombre_archivo = f"DMS_PROD_{timestamp}.txt"
 
     # Regex used to match all of the comments
     pattern = re.compile(r'^%%%%%TRAILER: (\w+)')
     trdPattern = re.compile(r'\s*,EXTENDED = TRUE')
-    matchSet = {}
 
     match = None 
-    sndMatch = None 
     strMatch = None
-    sndStrMatch = None
-    trdStrMatch = "EXTENDED = TRUE"
     
-    counter = 0
-    counterTwo = 0
     # Opening file
-    with open('datos/DMS.TXT', "r") as inputFile:
-        line = inputFile.readline()
-        while line:
-            counter +=1
-            if pattern.match(line):
-                match = pattern.match(line)
-                strMatch = match.group(1)
-            trdMatch = trdPattern.match(line)
-
-            if trdMatch:
-                counterTwo +=1
-                # matchSet.add()
-                print("Match: " + strMatch + " " + trdStrMatch + " " + str(counter) + " " + str(counterTwo))
-                
+    with open(nombre_archivo, "w") as outputFile:
+        with open('datos/DMS.TXT', "r") as inputFile:
             line = inputFile.readline()
+            while line:
+                if pattern.match(line):
+                    match = pattern.match(line)
+                    strMatch = match.group(1)
+                trdMatch = trdPattern.match(line)
 
-    # Add the input to the lex\
-    
+                if trdMatch:
+                        outputFile.write(strMatch + " EXTENDED" + '\n')
+                    
+                line = inputFile.readline()
 
 
 
